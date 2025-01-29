05f65efd4805546ba9f2cf4c42ad3a5d1964fdc8
yugaldhiman <yugaldhiman14@gmail.com>
2025-01-30 00:13:41 +0530

    create middle section

--- src/component/middle/MiddleFile.js
+++ src/component/middle/MiddleFile.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import './MiddleFile.css'
+import 'animate.css';
 
 const MiddleFile = () => {
     return (
         <>
-            <h1>middle</h1>
+            <div className='middleMainBox'>
+                <div className='middleMainBoxChild'>
+                    <ul>
+                        <li>Hello</li>
+                        <li>Hello</li>
+                        <li>Hello</li>
+                        <li>Hello</li>
+                        <li>Hello</li>
+                    </ul>
+                </div>
+            </div >
         </>
     )
 }
