e141797d0e4e36b6fc6b8118d63faebd0e283786
yugaldhiman <yugaldhiman14@gmail.com>
2025-02-03 21:38:18 +0530

    create about component

---
+++ src/component/about/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './About.css'
+
+const About = () => {
+    return (
+        <>
+            <div className='aboutMainBox'>
+                <h1>about</h1>
+                <div className='aboutMainBox-child'>
+                    <div className='aboutMainBox-childTopBox'></div>
+                    <div className='aboutMainBox-childBottomBox'></div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default About
